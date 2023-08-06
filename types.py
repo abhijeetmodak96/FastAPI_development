b10c790011237045143844b48352e3d87404ab38
Abhijeet Modak <abhijeet@Abhijeets-MacBook-Air.local>
2023-08-06 22:58:44 +0530

    added types.py file for learning python types

---
+++ types.py
@@ -0,0 +1,34 @@
+def get_full_name(first_name: str, last_name: str):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+
+def process_items(items: list[str]):
+    for item in items:
+        print(item)
+
+
+def process_items1(items_t: tuple[int, int, str], items_s: set[bytes]):
+    return items_t, items_s
+
+
+def process_items2(prices: dict[str, float]):
+    for item_name, item_price in prices.items():
+        print(item_name)
+        print(item_price)
+
+
+def process_item(item: int | str):
+    print(item)
+
+
+class Person:
+    def __init__(self, name: str):
+        self.name = name
+
+
+def get_person_name(one_person: Person):
+    return one_person.name
+
+
+print(get_full_name("john", "doe"))
